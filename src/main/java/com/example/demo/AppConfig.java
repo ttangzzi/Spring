6e152608696816778de259543feabc0e76e91c58
ttangzzi <cvbg0802@gmail.com>
2025-08-28 16:50:22 +0900

    AppConfig 리팩토링과 RateDiscountPolicy 적용

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
+import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
@@ -12,11 +15,24 @@ public class AppConfig {
     public MemberService memberService() {
         // 생성자를 통해서 MemoryMemberRepository 구현체를 선택한다.
         // 이것을 생성자를 통해 new instance가 들어간다고 해서 "생성자 주입"이라고 한다.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 리팩토링 : 역할들에 대한 구현을 한 눈에 들어오도록 하기.
+    // 나중에 DB 저장소로 바꾸게되면 이 코드만 바꾸면 된다.
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         // 생성자 주입을 통해 MemoryMemberRepository와 FixDiscountPolicy 구현체를 선택한다.
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 리팩토링 : 역할들에 대한 구현을 한 눈에 들어오도록 하기.
+    // 나중에 정률제로 바꾸게되면 이 코드만 바꾸면 된다.
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
